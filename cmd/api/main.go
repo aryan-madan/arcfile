6941851a1e7e65b5e6846b74800f7b6beceebbec
nxrmqlly <ritam.das3110@outlook.com>
2025-02-08 02:32:25 +0530

    add static pages

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,6 +12,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/nxrmqlly/arcfile-backend/handlers"
+	"github.com/nxrmqlly/arcfile-backend/public"
 	"github.com/nxrmqlly/arcfile-backend/ratelimits"
 	"github.com/nxrmqlly/arcfile-backend/storage"
 )
@@ -27,9 +29,7 @@ Author: nxrmqlly (Ritam Das)
 `
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
+	godotenv.Load()
 	fmt.Fprintf(os.Stdout, "\033[0;31m%s\033[0m\n\n", tag)
 
 	pool, err := storage.InitDatabase()
@@ -70,18 +70,18 @@ func main() {
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 10 << 20 // 10 MiB
 
-	router.Static("/static", "./static") // Serves static CSS and JS
-	router.LoadHTMLGlob("templates/*")   // Loads HTML templates
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
-	})
+	router.StaticFileFS("/about", "about.html", http.FS(public.Templates))
+	router.StaticFileFS("/", "app.html", http.FS(public.Templates))
 
 	router.POST("/api/upload", limiters["postFile"], handler.Upload)
 	router.GET("/api/file/:identifier", limiters["getFile"], handler.FileInfo)
 	router.GET("/api/download/:identifier", handler.FileDownload)
 
-	if err := router.Run("localhost:8080"); err != nil {
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run("localhost:" + port); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
